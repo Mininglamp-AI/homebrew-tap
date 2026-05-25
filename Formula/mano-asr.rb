8aaa2d44ed3347bce71424f8e598a3cc4b390c15
xiaoluo1009 <jiejiejie196@gmail.com>
2026-05-25 17:44:00 +0800

    Refactor ManoAsr installation and virtualenv usage
    Refactor installation process to use existing virtual environment and update site package paths.

--- Formula/mano-asr.rb
+++ Formula/mano-asr.rb
@@ -1,6 +1,4 @@
 class ManoAsr < Formula
-    include Language::Python::Virtualenv
-
     desc "本地语音转写服务，基于 MLX，针对 Apple Silicon 优化"
     homepage "https://github.com/Mininglamp-AI/mano-asr"
     url "https://github.com/Mininglamp-AI/mano-asr/archive/refs/tags/v0.1.0.tar.gz"
@@ -13,13 +11,20 @@ class ManoAsr < Formula
     depends_on :arch => :arm64
 
     def install
-      venv = virtualenv_create(libexec, "python3.13")
-      venv.pip_install_and_link buildpath
+      venv = libexec/"venv"
+      system "python3.13", "-m", "venv", venv
+      system venv/"bin/pip", "install", "--upgrade", "pip"
+      system venv/"bin/pip", "install", buildpath
 
-      site_packages = Dir[libexec/"lib/python*/site-packages"].first
+      site_packages = Dir[venv/"lib/python*/site-packages"].first
       cp_r "core", site_packages
       cp_r "utils", site_packages
       cp "server.py", site_packages
+
+      (bin/"mano-asr").write <<~SH
+        #!/bin/bash
+        exec "#{venv}/bin/python3" -m manoasr.cli.main "$@"
+      SH
     end
 
     def caveats
